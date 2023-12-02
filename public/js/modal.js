6e08883a76c70cd50166530e04fa48f4bf41ade8
lushkasuvorova <lushkasuvorova0@gmail.com>
2023-12-02 20:28:00 +0300

    new modal js

--- public/js/modal.js
+++ public/js/modal.js
@@ -19,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
         edits[i].addEventListener('show.bs.modal', event => {
             let Atributes = edits[i].querySelectorAll('.atribut');
             window.count_atributes = Atributes.length + 1
+            alert(window.count_atributes)
 
         })
 
@@ -62,14 +63,38 @@ function addAtribute(id) {
     const Atributes = document.getElementById(container)
     const div = document.createElement('div')
     div.id = 'atr' + window.count_atributes + editProductId
-    div.classList.add('newAtribut')
+    div.classList.add('newAtribut', 'container')
     Atributes.append(div)
+
+    const row1 = document.createElement('div')
+    row1.classList.add('row')
+    div.append(row1)
+    const row2 = document.createElement('div')
+    row2.classList.add('row')
+    div.append(row2)
+
+    const col11 = document.createElement('div')
+    col11.classList.add('col-md-5')
+    row1.append(col11)
+    const col12 = document.createElement('div')
+    col12.classList.add('col-md-5')
+    row1.append(col12)
+
+    const col21 = document.createElement('div')
+    col21.classList.add('col-md-5')
+    row2.append(col21)
+    const col22 = document.createElement('div')
+    col22.classList.add('col-md-5')
+    row2.append(col22)
+    const col23 = document.createElement('div')
+    col23.classList.add('col-md-1', 'my-auto')
+    row2.append(col23)
 
     const label_name = document.createElement('label')
     label_name.textContent = 'Название'
     label_name.for = 'data->atr_name' + window.count_atributes + editProductId
     label_name.classList.add('form-label');
-    div.append(label_name)
+    col11.append(label_name)
 
     const input_name = document.createElement('input')
     input_name.name = 'data->atr_name' + window.count_atributes + editProductId
@@ -77,19 +102,19 @@ function addAtribute(id) {
     input_name.classList.add('form-control');
     //показать номер
     //input_name.value = 'Цвет'
-    div.append(input_name)
+    col21.append(input_name)
 
     const label_value = document.createElement('label')
     label_value.textContent = 'Значение'
     label_value.for = 'data->atr_value' + window.count_atributes + editProductId
     label_value.classList.add('form-label');
-    div.append(label_value)
+    col12.append(label_value)
 
     const input_value = document.createElement('input')
     input_value.name = 'data->atr_value' + window.count_atributes + editProductId
     input_value.type = 'text'
     input_value.classList.add('form-control');
-    div.append(input_value)
+    col22.append(input_value)
 
     const del_atr = document.createElement('a')
     //del_atr.href = '#'
@@ -98,10 +123,12 @@ function addAtribute(id) {
     del_atr.onclick = function delAtribute() {
         const delAtribute = document.getElementById('atr' + this.id)
         delAtribute.remove()
+        alert('del'+ this.id)
     }
     //del_atr.textContent = 'удалить атрибут'
-    div.append(del_atr)
+    col23.append(del_atr)
     del_atr.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16" style="color: gray;">  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>  <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg>'
 
     window.count_atributes++
+    alert('add'+ (window.count_atributes-1))
 }
